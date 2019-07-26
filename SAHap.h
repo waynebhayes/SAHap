d74456e1d9215eeccca6ffde6481c954bdf8152b
waynebhayes <whayes@uci.edu>
2019-07-25 23:11:53 -0700

    updated data structure

--- SAHap.h
+++ SAHap.h
@@ -1,32 +1,18 @@
 #ifndef __SAHAP_H__
 
-extern int Slen;
-extern char *S; // current best solution
+#define K 2 // ploidy (number of chromosomes)
+extern int m; // number of SNP positions
+extern char *S[K]; // current best solution
 typedef struct _readPlace READ_PLACEMENT;
 
 typedef struct _ReadPair {
-    char *seq[2]; // two sequences of characters, ie., ACGT's
-    int gap, len[2]; // estimated gap between, and precise lengths of, the two sequences
-    READ_PLACEMENT *currentPlace;
+    // CONSTANTS: these don't change once the read is created.
+    int gap; // estimated gap between the two ends (may not be used)
+    int pos[2]; // the starting positions [0,..m) of the two reads; assert(pos[1] > pos[0]+len[0]);
+    int len[2]; // lengths of the two reads
+    char *seq[2]; // the actual sequences, of length len[0] and len[1]
+    // Above are all constants. Below is our one variable.
+    // GOAL VARIABLE: our goal is to figure out the correct value of "which", meaning which chromosome does this come from?
+    char which; // which chromosome are we currently associated with?? 0 through K-1
 } READ_PAIR;
-
-typedef struct _readPlace {
-    READ_PAIR *R; // the read we are referring to
-
-    // Orientation: it must be +1 or -1, meaning the read is placed forward or backward along the sequence
-    int orientation;
-
-    unsigned int loc[2]; // loc is always positive. However...
-    // if orientation is +1:
-	// loc[0] is the left-most location in S[] of R->seq[0][0], and
-	// loc[1] is the left-most location in S[] of R->seq[1][0].
-    // if orientation is -1:
-	// R->seq[0][i] is at S[Slen-1-loc[0]-i].
-	// R->seq[1][i] is at S[Slen-1-loc[1]-i].
-    // Note also that the objective function should probably have some element of preferring that
-    // ABS(loc[1]-loc[0]) is close to R->gap, so for example penalize by ABS(ABS(loc[1]-loc[0])-gap),
-    // although that may be too much.
-} READ_PLACEMENT;
-
-#endif
 
