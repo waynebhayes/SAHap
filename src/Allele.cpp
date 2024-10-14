1db44939452dd1f33eaba288ca633cc6a1d96dc1
Arnav Dhariya <adhariya518@student.fuhsd.org>
2024-10-14 11:50:33 -0700

    Update Allele.cpp

--- src/Allele.cpp
+++ src/Allele.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 namespace SAHap {
 
+//Expects: allele of type Allele
+//Returns: size_t of allele 
 size_t allele_i(Allele allele) {
 	return (size_t)allele;
 }
-
+//Expects: Allele l 
+//Return: if known, prints the integer at allele l, otherwise prints 'X'  
+//
+//Question: what is l?
 ostream & operator << (ostream& stream, const Allele& l) {
 	if (l == Allele::UNKNOWN) stream << 'X';
 	else stream << (int)l;
